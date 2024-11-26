dfcda593af4d1f69717c15a8c4f92678b8c5069b
AneriPanchal <aneAnerianeri@o365.siprtc.io>
2024-11-26 04:41:07 -0700

    model updated

--- models/event.go
+++ models/event.go
@@ -2,13 +2,12 @@ package models
 
 import (
 	"time"
-
 	//"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Event struct {
 	ID          string    `json:"id" bson:"_id,omitempty"`
-	Title       string    `json:"title" validate:"required"`
-	Description string    `json:"description" validate:"required"`
-	Date        time.Time `json:"date" validate:"required"`
+	Title       string    `json:"title" bson:"title" validate:"required"`
+	Description string    `json:"description" bson:"description" validate:"required"`
+	Date        time.Time `json:"date" bson:"date" validate:"required"`
 }
